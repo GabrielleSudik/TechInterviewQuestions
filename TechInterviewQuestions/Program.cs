3535777690bed5bfb9bea343503c4749ded138d6
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-09-13 19:28:40 -0400

    more added

--- TechInterviewQuestions/Program.cs
+++ TechInterviewQuestions/Program.cs
@@ -88,7 +88,80 @@ namespace TechInterviewQuestions
                 IsPrime = true;
             }
 
+            //here's another test:
+
+            //create an array of whatever size integers.
+            //method should return largest number in the array
+
+            //whee you got this one on your own!
+            //well... you didn't put it in a method, but you got it
+
+            Console.WriteLine();
+
+            int[] array =         
+             { 2, 3, 5, 7, 11, 13, 17, 19, 23, 31, 37, 43, 41 };
+
+            int biggestNumber = 0;
+
+            Console.WriteLine(array[12]);
+
+            for (int i = 0; i < array.Length-1 ; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    biggestNumber = array[i];
+                }
+
+                else
+                {
+                    biggestNumber = array[i + 1];           }
+            }
+
+            Console.WriteLine($"Biggest number is {biggestNumber}");
+
+            //there are a lot of ways to do this problem
+            //there's an example on slack (photo of white board)
+
+            //public int LargestNumber (int[] array)
+
+            //...
+            //return biggestNumber;
+
+
+            //create a method that takes a string parameter
+            //then returns that string reversed.
+            //DO NOT use .Reverse
+
+            Console.WriteLine();
+
+            string name = "GABRIELLE";
+
+            for (int i = 0; i <name.Length; i++)
+            {
+                Console.Write(name[name.Length-i-1]);
+            }
+
+            //NameReversed(name);
+
+            //Console.WriteLine(reversed);
+
             Console.ReadLine();
         }
+
+        public void NameReversed (string name)
+        {
+            string reversedName = "";
+
+            for (int i = 0; i < name.Length; i++)  //could also set (1 = name.Length - 1; i >=0; i--)
+            {
+                Console.Write(name[name.Length - i - 1]);
+            }
+
+            //here's a thing: you can += with chars in a string!
+            //like:   reversedName += name[i]
+
+            //then return reversedName;
+            
+        }
+        }
     }
-}
